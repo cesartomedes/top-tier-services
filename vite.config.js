fa466b475f193151288f5e1561202fb497a49c6a
cesartomedes <cesartomedes@gmail.com>
2026-03-24 21:31:33 -0400

    fix route

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Esto asegura que las rutas sean absolutas desde la raíz
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  }
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
